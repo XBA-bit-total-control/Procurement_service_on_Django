67780acf127f1b39cf83c4937f8354e31f0655d1
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-04 14:47:52 +0300

    Authentication settings are set Установлены настройки для аутентификации

--- purchasing_service/purchasing_service/settings.py
+++ purchasing_service/purchasing_service/settings.py
@@ -135,3 +135,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASS": [
+        "rest_framework.authentication.TokenAuthentication"
+    ]
+}
