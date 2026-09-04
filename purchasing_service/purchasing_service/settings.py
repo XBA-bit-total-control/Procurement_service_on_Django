1a870d96668e6da7c7f9ed14052713da8a1e2d7a
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-04 14:50:06 +0300

    Settings have been made for working with email Внесены настройки для работы с email

--- purchasing_service/purchasing_service/settings.py
+++ purchasing_service/purchasing_service/settings.py
@@ -144,3 +144,15 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication"
     ]
 }
+
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_USE_SSL = not os.getenv("EMAIL_USE_SSL")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
