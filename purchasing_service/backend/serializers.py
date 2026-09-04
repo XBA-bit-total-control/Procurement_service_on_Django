2e55e124af49b610fb352d8e0291ec4afd8dcf97
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-04 14:42:36 +0300

    To create correct authentication using email, a custom serializer was created Для создания корректной аутентификации с использованием email был создан кастомный сериализатор

--- purchasing_service/backend/serializers.py
+++ purchasing_service/backend/serializers.py
@@ -4,6 +4,10 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.serializers import ValidationError
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
@@ -82,3 +86,31 @@ class UserSerializer(serializers.Serializer):
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
         return User.objects.create(**validated_data)
+
+
+class CustomAuthTokenSerializer(AuthTokenSerializer):
+    username = None
+    email = serializers.EmailField()
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'),
+                                email=email, password=password)
+            if not user:
+                msg = _('Unable to log in with provided credentials. '
+                        'Check the spelling of the email and password.')
+                raise ValidationError(msg, code='authorization')
+
+            if not user.is_confirm:
+                msq = _("Users with an unconfirmed email cannot receive an authentication token. "
+                        "Complete the registration by confirming your email.")
+                raise ValidationError(msq, code='authorization')
+        else:
+            msg = _('Must include "email" and "password".')
+            raise ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
