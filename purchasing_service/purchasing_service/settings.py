42c053c9b2636cebc156c9f9e65631bf0db43658
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-07 18:00:56 +0300

    The name of the key in REST_FRAMEWORK has been corrected for correct authentication Название ключа в REST_FRAMEWORK исправлено для корректной аутентификации

--- purchasing_service/purchasing_service/settings.py
+++ purchasing_service/purchasing_service/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "backend.User"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASS": [
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
     ]
 }
