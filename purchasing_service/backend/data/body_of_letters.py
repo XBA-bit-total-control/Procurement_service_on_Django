0b85bd6bb1f25d0ffa581a4049af8e57119f8c30
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-09 18:26:38 +0300

    Functions have been created that return the text of an email confirming an order, both for the user and for the administrator Созданы функции, которые возвращают текст email с подтверждением заказа как для пользователя, так и для админа

--- purchasing_service/backend/data/body_of_letters.py
+++ purchasing_service/backend/data/body_of_letters.py
@@ -9,3 +9,27 @@ def completing_registration(token: str) -> str:
     
     Your service for Compraretis orders.
     """
+
+
+def order_created_for_user(
+        order_id: int,
+        order_price: float,
+        username: str
+) -> str:
+    return f"""
+    {username.capitalize()} hello!
+    Your order for the amount of {order_price} rubles has been accepted for processing and has the number {order_id}
+    Thank you for your purchase!
+    """
+
+
+def order_created_for_admin(
+        order_id: int,
+        order_price: float,
+        username: str,
+        user_id: int
+) -> str:
+    return f"""
+    The user {username} with id={user_id} wants to place an order with order id={order_id} for the amount {order_price}
+    Confirmation is required.
+    """
