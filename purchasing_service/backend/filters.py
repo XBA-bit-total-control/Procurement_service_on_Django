449355499af3c619e0c97075db276d60e347ff1a
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-10 17:17:05 +0300

    A filter class has been created for ProductInfoFilter Для ProductInfoFilter создан класс-фильтр

--- purchasing_service/backend/filters.py
+++ purchasing_service/backend/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 
-from .models import User, Shop, Category
+from .models import User, Shop, Category, ProductInfo
 
 
 class UserFilter(django_filters.FilterSet):
@@ -66,4 +66,57 @@ class CategoryFilter(django_filters.FilterSet):
 
     class Meta:
         model = Category
+        fields = []
+
+
+class ProductInfoFilter(django_filters.FilterSet):
+    id = django_filters.NumberFilter(field_name='id')
+    product_id = django_filters.NumberFilter(field_name='product_id')
+    shop_id = django_filters.NumberFilter(field_name='shop_id')
+    category_id = django_filters.NumberFilter(field_name='product__category_id')
+
+    model = django_filters.CharFilter(field_name='model')
+    model_contains = django_filters.CharFilter(
+        field_name='model',
+        lookup_expr='contains'
+    )
+
+    name = django_filters.CharFilter(field_name='name')
+    name_contains = django_filters.CharFilter(
+        field_name='name',
+        lookup_expr='contains'
+    )
+
+    quantity = django_filters.NumberFilter(field_name='quantity')
+    quantity_less = django_filters.NumberFilter(
+        field_name='quantity',
+        lookup_expr='lt'
+    )
+    quantity_more = django_filters.NumberFilter(
+        field_name='quantity',
+        lookup_expr='gt'
+    )
+
+    price = django_filters.NumberFilter(field_name='price')
+    price_less = django_filters.NumberFilter(
+        field_name='price',
+        lookup_expr='lt'
+    )
+    price_more = django_filters.NumberFilter(
+        field_name='price',
+        lookup_expr='gt'
+    )
+
+    price_rrc = django_filters.NumberFilter(field_name='price_rrc')
+    price_rrc_less = django_filters.NumberFilter(
+        field_name='price_rrc',
+        lookup_expr='lt'
+    )
+    price_rrc_more = django_filters.NumberFilter(
+        field_name='price_rrc',
+        lookup_expr='gt'
+    )
+
+    class Meta:
+        model = ProductInfo
         fields = []
