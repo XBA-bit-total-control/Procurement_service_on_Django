5a8f47c5f2e44268474f6da9cb21d0cdc3d4d4f2
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-16 19:20:29 +0300

    The celery module has been created in the main package of the project and the creation of the Celery application is configured. В главном пакете проекта создан модуль celery.py и настроено создание приложения Celery

---
+++ purchasing_service/purchasing_service/celery.py
@@ -0,0 +1,10 @@
+import os
+
+from celery import Celery
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purchasing_service.settings')
+
+celery_app = Celery('purchasing_service')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.autodiscover_tasks()
