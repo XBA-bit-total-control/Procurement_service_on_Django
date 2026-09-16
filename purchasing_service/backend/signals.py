cf507a052bfbc0b71e419a35e680594e1ac50644
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-16 19:33:01 +0300

    For signal processing and correct operation of django_rest_passwordreset, the signals module and handler were created Для обработки сигналов и корректной работы django_rest_passwordreset был создан модуль signals и обработчик

---
+++ purchasing_service/backend/signals.py
@@ -0,0 +1,18 @@
+from django.dispatch import receiver
+from django_rest_passwordreset.signals import reset_password_token_created
+
+from .data.body_of_letters import reset_password
+from .tasks import send_email
+
+
+@receiver(reset_password_token_created)
+def email_about_password_change(reset_password_token, *args, **kwargs):
+    email = reset_password_token.user.email
+    first_name = reset_password_token.user.first_name
+    token = reset_password_token.key
+
+    send_email.delay(
+        subject="Compraretis service: Password reset",
+        recipient=email,
+        content=reset_password(first_name, token)
+    )
