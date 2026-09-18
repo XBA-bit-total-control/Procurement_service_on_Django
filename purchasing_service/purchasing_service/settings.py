380600f919e16d1e33aba93e153e929d10636b0d
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-18 15:18:05 +0300

    Added settings for limiting the number of requests per minute Добавлены настройки ограничения количества запросов в минуту

--- purchasing_service/purchasing_service/settings.py
+++ purchasing_service/purchasing_service/settings.py
@@ -147,7 +147,15 @@ AUTH_USER_MODEL = "backend.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
-    ]
+    ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+        "rest_framework.throttling.AnonRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "user": "20/minute",
+        "anon": "10/minute"
+    }
 }
 
 
