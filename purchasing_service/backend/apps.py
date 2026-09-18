3fb0d26915355b568bee12f879213027e44fb6c5
Vladislav Huc <vladislavhucisvili@gmail.com>
2026-09-18 14:48:25 +0300

    Added dockstring to the apps module В модуль apps добавлены dockstring

--- purchasing_service/backend/apps.py
+++ purchasing_service/backend/apps.py
@@ -2,8 +2,20 @@ from django.apps import AppConfig
 
 
 class BackendConfig(AppConfig):
+    """Конфигурация приложения Backend.
+
+    Задает настройки приложения и подключает сигналы.
+
+    Attributes:
+        default_auto_field: тип автоинкрементного поля для моделей
+        name: название приложения
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend'
 
     def ready(self):
+        """Инициализация приложения.
+
+        Подключает модуль signals для обработки сигналов.
+        """
         from . import signals
